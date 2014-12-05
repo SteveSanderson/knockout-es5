e7a47e039fa6ad93f561f32cdaf3cff76801eb51
Constantine Melnikov <ka.melnikov@gmail.com>
2014-12-05 12:47:22 +0800

    fix watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,7 @@ module.exports = function(grunt) {
 
     watch: {
       scripts: {
-        files: ['src/*.js', 'spec/*.js'],
+        files: ['src/*.js', 'test/*.js'],
         tasks: ['default'],
         options: {
           nospawn: false
