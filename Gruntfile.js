c313d821a9606499b18ba510ae865971dfaddeb0
Constantine Melnikov <ka.melnikov@gmail.com>
2014-12-15 21:56:46 +0800

    update gruntfile, add jshint to travis task

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('test', ['karma:local']);
   grunt.registerTask('build', ['concat', 'uglify']);
-  grunt.registerTask('travis', ['build', 'karma:sauce']);
+  grunt.registerTask('travis', ['jshint', 'build', 'karma:sauce']);
   grunt.registerTask('default', ['jshint', 'build', 'test']);
 
 };
