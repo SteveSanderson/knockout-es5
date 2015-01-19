fd49f340242e748eae5c533c094f02aaf1b530b3
Constantine Melnikov <ka.melnikov@gmail.com>
2015-01-19 22:56:55 +0800

    Fix tests. Since ko 3.x computed behavior has changed, see the comments to the tests.
    Since ko 3.1.0, Deferred computeds will now be evaluated on a manual subscription, if not previously evaluated.
    
    Since ko 3.0.0 Computed properties now notify only when their value changes.

--- test/computed-properties.test.js
+++ test/computed-properties.test.js
@@ -3,69 +3,81 @@ describe('Computed properties', function () {
   describe('ko.trackComputed()', function () {
 
     it('adds a gettable property that notifies on change', function() {
-      var model = { prop: 100 },
-        timesEvaluated = 0,
-        notifiedValues = [];
+      var model = {
+        prop: 100
+      };
+      var timesEvaluated = 0;
+      var notifiedValues = [];
 
       ko.defineProperty(model, 'propPlusOne', function() {
         timesEvaluated++;
         return model.prop + 1;
       });
-      ko.getObservable(model, 'propPlusOne').subscribe(function(notifiedValue) {
-        notifiedValues.push(notifiedValue);
-      });
 
       // Important: defineProperty can be called *before* its dependencies are tracked (because evaluation is deferred)
-      ko.track(model);
+      // But, Since ko 3.1.0, Deferred computeds will now be evaluated on a manual subscription, if not previously evaluated
+      // @see https://github.com/knockout/knockout/pull/1272
+      ko.track( model );
 
-      // Evaluation should be deferred until first read
-      assert.equal( timesEvaluated, 0);
-      assert.equal( notifiedValues.length, 0);
+      // All subscriptions must be after ko.track()
+      ko.getObservable(model, 'propPlusOne').subscribe(function( notifiedValue ) {
+        notifiedValues.push( notifiedValue );
+      });
 
-      // First read triggers evaluation and hence a notification
+      assert.equal( timesEvaluated, 1 );
+      assert.equal( notifiedValues.length, 0 );
+
+      // Since ko 3.0.0 Computed properties now notify only when their value changes
       assert.equal( model.propPlusOne, 101);
       assert.equal( timesEvaluated, 1);
-      assert.equal( notifiedValues.length, 1);
-      assert.equal( notifiedValues[ 0 ], 101);
+      assert.equal( notifiedValues.length, 0);
 
       // Subsequent reads don't trigger evaluation
       assert.equal( model.propPlusOne, 101);
       assert.equal( timesEvaluated, 1);
-      assert.equal( notifiedValues.length, 1);
-      assert.equal( notifiedValues[ 0 ], 101); // No additional notification
+      assert.equal( notifiedValues.length, 0); // No notification
 
       // Modifying a dependency triggers re-evaluation and notification
       model.prop = 200;
       assert.equal( timesEvaluated, 2); // Evaluation happens regardless of read
-      assert.equal( notifiedValues.length, 2);
-      assert.equal( notifiedValues[ 0 ], 101);
-      assert.equal( notifiedValues[ 1 ], 201);
+      assert.equal( notifiedValues.length, 1);
+      assert.equal( notifiedValues[ 0 ], 201);
       assert.equal( model.propPlusOne, 201);
     });
 
     it('accepts an Object.defineProperty-like options object to allow further control (e.g., for a setter)', function() {
-      var model = { prop: 100 },
-        timesEvaluated = 0,
-        notifiedValues = [];
+      var model = {
+        prop: 100
+      };
+      var timesEvaluated = 0;
+      var notifiedValues = [];
+
       ko.defineProperty(model, 'propPlusOne', {
-        get: function() { timesEvaluated++; return model.prop + 1; },
-        set: function(value) { model.prop = value - 1; }
+        get: function() {
+          timesEvaluated++;
+          return model.prop + 1;
+        },
+        set: function(value) {
+          model.prop = value - 1;
+        }
       });
-      ko.getObservable(model, 'propPlusOne').subscribe(function(notifiedValue) { notifiedValues.push(notifiedValue); });
-      ko.track(model);
+
+      ko.track( model );
+
+      ko.getObservable(model, 'propPlusOne').subscribe(function( notifiedValue ) {
+        notifiedValues.push( notifiedValue );
+      });
 
       assert.equal( model.propPlusOne, 101);
       assert.equal( timesEvaluated, 1);
-      assert.equal( notifiedValues.length, 1);
-      assert.equal( notifiedValues[ 0 ], 101);
+      assert.equal( notifiedValues.length, 0);
 
       // Now check the setter works
       model.propPlusOne = 200;
       assert.equal( model.prop, 199);
       assert.equal( timesEvaluated, 2);
-      assert.equal( notifiedValues.length, 2);
-      assert.equal( notifiedValues[ 0 ], 101);
-      assert.equal( notifiedValues[ 1 ], 200);
+      assert.equal( notifiedValues.length, 1);
+      assert.equal( notifiedValues[ 0 ], 200);
       assert.equal( model.propPlusOne, 200);
     });
 
